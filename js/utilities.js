ed6969d1c6e26b1bddf61210c5e3ea3e3843b57c
Rakibul Islam <rakibul241221@gmail.com>
2022-09-11 13:10:50 +0600

    first commit

---
+++ js/utilities.js
@@ -0,0 +1,25 @@
+function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  inputField.value = "";
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  if (isNaN(inputFieldValue)) {
+    return alert('Provide valid info')
+  }
+  return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.innerText;
+  const textElementValue = parseFloat(textElementValueString);
+  if (isNaN(textElementValue)) {
+    return alert('Provide valid info')
+  }
+  return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
